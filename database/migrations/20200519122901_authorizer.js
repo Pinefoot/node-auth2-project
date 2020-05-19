4dabdddba2e2f1f6843b0c76591f03c7ccc15bb8
Pinefoot <jameslcarpino@gmail.com>
2020-05-19 14:51:35 -0700

    added more files

--- database/migrations/20200519122901_authorizer.js
+++ database/migrations/20200519122901_authorizer.js
@@ -11,6 +11,7 @@ exports.up = function(knex) {
       tbl.increments()
       tbl.string('username', 128).notNullable().unique().index()
       tbl.string('password', 256).notNullable();
+      tbl.string('department', 128).notNullable();
 
       tbl.integer('role')
       .unsigned()
